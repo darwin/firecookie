ab412395741c8b88dd400d245dbcb7cd7276c992
odvarko@gmail.com <odvarko@gmail.com@c6f3a21d-8651-0410-bb1f-c71b198f148d>
2009-02-26 19:04:17 +0000

    New API for FBTest.FireCookie namespace.
    git-svn-id: http://firecookie.googlecode.com/svn/trunk@108 c6f3a21d-8651-0410-bb1f-c71b198f148d

--- tests/env.js
+++ tests/env.js
@@ -8,7 +8,29 @@ var FBTestFireCookie = FBTest.FireCookie =
 enableCookiePanel: function(callback)
 {
     FBTestFirebug.updateModelPermission(FW.Firebug.FireCookieModel, callback, "enable");
-}
+},
+
+getCookieRowByName: function(panelNode, cookieName)
+{
+    var cookieRows = FW.FBL.getElementsByClass(panelNode, "cookieRow");
+    for (var i=0; i<cookieRows.length; i++)
+    {
+        var row = cookieRows[i];
+        var label = FW.FBL.getElementsByClass(row, "cookieNameLabel");
+        if (label.length != 1)
+            return null;
+
+        if (label[0].textContent == cookieName)
+            return row; 
+    }
+    return null;
+},
+
+getCookieByName: function(panelNode, cookieName)
+{
+    var row = this.getCookieRowByName(panelNode, cookieName);
+    return row ? row.repObject : null;
+},
 
 // ************************************************************************************************
 };
