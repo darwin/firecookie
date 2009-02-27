ac3fc5e2aad2a01cd80989919f29834741db2e7e
odvarko@gmail.com <odvarko@gmail.com@c6f3a21d-8651-0410-bb1f-c71b198f148d>
2009-02-27 08:39:51 +0000

    Create new FBTestFireCookie.editCookie method and update test issue 34.
    git-svn-id: http://firecookie.googlecode.com/svn/trunk@112 c6f3a21d-8651-0410-bb1f-c71b198f148d

--- tests/env.js
+++ tests/env.js
@@ -1,16 +1,21 @@
 /* See license.txt for terms of usage */
 
-var FBTestFireCookie = FBTest.FireCookie = 
-{
-// ************************************************************************************************
-// Firecookie specific APIs
+(function() {
 
-enableCookiePanel: function(callback)
+// ************************************************************************************************
+// Constants
+
+var winWatcher = Cc["@mozilla.org/embedcomp/window-watcher;1"].getService(Ci.nsIWindowWatcher);
+
+// ************************************************************************************************
+// Firecookie testing APIs
+
+this.enableCookiePanel = function(callback)
 {
     FBTestFirebug.updateModelPermission(FW.Firebug.FireCookieModel, callback, "enable");
-},
+};
 
-getCookieRowByName: function(panelNode, cookieName)
+this.getCookieRowByName = function(panelNode, cookieName)
 {
     var cookieRows = FW.FBL.getElementsByClass(panelNode, "cookieRow");
     for (var i=0; i<cookieRows.length; i++)
@@ -24,13 +29,48 @@ getCookieRowByName: function(panelNode, cookieName)
             return row; 
     }
     return null;
-},
+};
 
-getCookieByName: function(panelNode, cookieName)
+this.getCookieByName = function(panelNode, cookieName)
 {
     var row = this.getCookieRowByName(panelNode, cookieName);
     return row ? row.repObject : null;
-},
+};
+
+/**
+ * Opens editCookie.xul dialog. Since the dialog is modal, the method returns
+ * after its closed. Use callback to close the dialog.
+ * 
+ * @param {Object} cookie Cookie beeing edited
+ * @param {Object} callback Callback for dialog manipulation.
+ */
+this.editCookie = function(cookie, callback)
+{
+    var watcherObserver =
+    {
+        observe: function(subject, topic, data)
+        {
+            if (topic == "domwindowopened")
+            {
+                winWatcher.unregisterNotification(watcherObserver);
+                setTimeout(function()
+                {
+                    var dialog = subject.QueryInterface(Ci.nsIDOMWindow);
+                    FBTest.compare("chrome://firecookie/content/editCookie.xul",
+                        dialog.document.location.href, "The editCookie.xul is opened.");
+                    FBTest.ok(dialog.EditCookie, "The EditCookie varible must exist");
+                    callback(dialog);
+                }, 300);
+            }
+        }
+    };
+
+    winWatcher.registerNotification(watcherObserver);
+    return FW.Firebug.FireCookieModel.Templates.CookieRow.onEdit(cookie);
+};
 
 // ************************************************************************************************
-};
+}).apply(FBTest.FireCookie);
+
+// Shortcut
+var FBTestFireCookie = FBTest.FireCookie; 
--- tests/issue34.js
+++ tests/issue34.js
@@ -1,5 +1,3 @@
-var winWatcher = Cc["@mozilla.org/embedcomp/window-watcher;1"].getService(Ci.nsIWindowWatcher);
-
 function runTest()
 {
     FBTest.sysout("cookies.test.issue34; START");
@@ -11,51 +9,32 @@ function runTest()
         FBTestFireCookie.enableCookiePanel(function(win) 
         {
             var panelNode = FBTestFirebug.selectPanel("cookies").panelNode;
-            var cookie = FBTestFireCookie.getCookieByName(panelNode, "TestCookie34");
-            FBTest.ok(cookie, "Cookie 'TestCookie34' must exist.");
-            if (!cookie)
-                return FBTestFirebug.testDone();
+            var cookie1 = FBTestFireCookie.getCookieByName(panelNode, "TestCookie34-1");
+            var cookie2 = FBTestFireCookie.getCookieByName(panelNode, "TestCookie34-2");
 
-            FBTest.sysout("cookies.test.issue34; this is our cookie", cookie);
+            editCookie(cookie1);
+            editCookie(cookie2);
 
-            // Check domain
-            FBTest.compare(".softwareishard.com", cookie.cookie.host, "Check host.");
-
-            // The edit cookie dialog is modal so, we have to register a listener
-            // to finish the test. 
-            winWatcher.registerNotification(watcherObserver);
-
-            // Open editCookie.xul dialog and edit the value.
-            FBTest.sysout("cookies.test.issue34; let's edit an existing cookie");
-            FW.Firebug.FireCookieModel.Templates.CookieRow.onEdit(cookie);
-            FBTest.sysout("cookies.test.issue34; editCookie.xul closed");
-
-            // Now the dialog has been finished so, check the new value.
-            FBTest.compare("NewValueForCookie34", cookie.cookie.value, "Check new cookie value");
-            //FBTestFirebug.testDone("cookies.test.issue34; DONE");
+            FBTest.compare("ValueCookie34-modified", cookie1.cookie.value, "Check new cookie value");
+            FBTest.compare("ValueCookie34-modified", cookie2.cookie.value, "Check new cookie value");
+            FBTestFirebug.testDone("cookies.test.issue34; DONE");
         });
     });
 };
 
-var watcherObserver =
+function editCookie(cookie)
 {
-    observe: function(subject, topic, data)
-    {
-        if (topic == "domwindowopened")
-        {
-            var win = subject.QueryInterface(Ci.nsIDOMWindow);
-            setTimeout(function() 
-            {
-                FBTest.sysout("cookies.test.issue34; editCookie.xul opened", win);
-                FBTest.compare("chrome://firecookie/content/editCookie.xul",
-                    win.document.location.href, "The window must match");
-                FBTest.ok(win.EditCookie, "The EditCookie varible must exist");
+    FBTest.ok(cookie, "Cookie must exist.");
+    if (!cookie)
+        return;
 
-                // Change value and finish the dialog.
-                win.EditCookie.nameNode.value = "NewValueForCookie34";
-                win.EditCookie.onOK();
-            }, 200);
-            winWatcher.unregisterNotification(watcherObserver);
-        }
-    }
-};
+    FBTest.sysout("cookies.test.issue34; this is our cookie", cookie);
+    FBTest.compare(".softwareishard.com", cookie.cookie.host, "Check cookie host.");
+
+    // Open editCookie.xul dialog and edit the value.
+    FBTest.sysout("cookies.test.issue34; let's edit an existing cookie");
+    return FBTestFireCookie.editCookie(cookie, function(dialog) {
+        dialog.EditCookie.valueNode.value = cookie.cookie.value + "-modified";
+        dialog.EditCookie.onOK();
+    });
+}
