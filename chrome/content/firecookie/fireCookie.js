fa5d74b6e9c70c61cdab63b5464becdf84b9935e
odvarko@gmail.com <odvarko@gmail.com@c6f3a21d-8651-0410-bb1f-c71b198f148d>
2009-10-12 12:30:25 +0000

    Properly remove bp from the UI.
    git-svn-id: http://firecookie.googlecode.com/svn/trunk@236 c6f3a21d-8651-0410-bb1f-c71b198f148d

--- chrome/content/firecookie/fireCookie.js
+++ chrome/content/firecookie/fireCookie.js
@@ -4703,16 +4703,27 @@ Firebug.FireCookieModel.BreakpointTemplate = domplate(Firebug.Rep,
         var bpPanel = Firebug.getElementPanel(event.target);
         var context = bpPanel.context;
 
-        if (hasClass(event.target, "closeButton"))
-        {
-            // Remove from list of breakpoints.
-            var row = getAncestorByClass(event.target, "breakpointRow");
-            context.cookies.breakpoints.removeBreakpoint(row.repObject);
+        if (!hasClass(event.target, "closeButton"))
+            return;
 
-            // Remove from the UI.
-            bpPanel.noRefresh = true;
-            bpPanel.removeRow(row);
-            bpPanel.noRefresh = false;
+        // Remove from list of breakpoints.
+        var row = getAncestorByClass(event.target, "breakpointRow");
+        context.cookies.breakpoints.removeBreakpoint(row.repObject);
+
+        // Remove from the UI.
+        bpPanel.noRefresh = true;
+        bpPanel.removeRow(row);
+        bpPanel.noRefresh = false;
+
+        var cookiePanel = context.getPanel(panelName, true);
+        if (!cookiePanel)
+            return;
+
+        var cookie = cookiePanel.findRepObject(row.repObject);
+        if (cookie)
+        {
+            cookie.row.removeAttribute("breakpoint");
+            cookie.row.removeAttribute("disabledBreakpoint");
         }
     },
 
