f0fa2afda3757436971e9da45a17dc085946a8c9
odvarko@gmail.com <odvarko@gmail.com@c6f3a21d-8651-0410-bb1f-c71b198f148d>
2010-06-12 09:38:03 +0000

    FBTest for break on cookie modification.
    git-svn-id: http://firecookie.googlecode.com/svn/trunk@289 c6f3a21d-8651-0410-bb1f-c71b198f148d

---
+++ chrome/content/firecookie/tests/cookieBreakOnNext.js
@@ -0,0 +1,34 @@
+function runTest()
+{
+    FBTest.sysout("cookies.test.breakOnNext; START");
+    FBTest.loadScript("env.js", this);
+
+    FBTestFirebug.openNewTab(basePath + "general/breakOnNext.php", function(win)
+    {
+        FBTestFireCookie.enableCookiePanel(function(win)
+        {
+            var panelNode = FBTestFirebug.selectPanel("cookies").panelNode;
+
+            // xxxHonza TODO:
+
+            FBTestFirebug.testDone("cookies.test.breakOnNext; DONE");
+        });
+    });
+};
+
+function clickBreakOnCookie()
+{
+    var chrome = FW.Firebug.chrome;
+    FBTestFirebug.clickBreakOnNextButton(chrome);
+}
+
+function waitForBreakOnCookie(lineNo, breakpoint, callback)
+{
+    var chrome = FW.Firebug.chrome;
+    FBTestFirebug.waitForBreakInDebugger(chrome, lineNo, breakpoint, function(sourceRow)
+    {
+        FBTest.sysout("net.breakpoints; Break on Cookie OK");
+        FBTestFirebug.clickContinueButton(chrome);
+        callback();
+    });
+}
