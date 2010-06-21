a3077b14c3163c1b79ed95a3699cc2648f11ac28
odvarko@gmail.com <odvarko@gmail.com@c6f3a21d-8651-0410-bb1f-c71b198f148d>
2010-06-21 10:55:37 +0000

    Fix break on cookie change.
    git-svn-id: http://firecookie.googlecode.com/svn/trunk@296 c6f3a21d-8651-0410-bb1f-c71b198f148d

--- chrome/content/firecookie/fireCookie.js
+++ chrome/content/firecookie/fireCookie.js
@@ -4969,18 +4969,23 @@ Firebug.FireCookieModel.Breakpoints =
 
         Firebug.Debugger.halt(function(frame)
         {
+            if (FBTrace.DBG_COOKIES)
+                FBTrace.sysout("cookies.breakNow; debugger halted");
+
             for (; frame && frame.isValid; frame = frame.callingFrame)
             {
                 var fileName = frame.script.fileName;
                 if (fileName &&
                     fileName.indexOf("chrome://firebug/") != 0 &&
                     fileName.indexOf("chrome://firecookie/") != 0 &&
-                    fileName.indexOf("/components/firebug-") == -1)
+                    fileName.indexOf("/components/firebug-") == -1 &&
+                    fileName.indexOf("/modules/firebug-") == -1)
                     break;
             }
 
             if (frame)
             {
+                Firebug.Debugger.breakContext = context;
                 Firebug.Debugger.onBreak(frame, 3);
             }
             else
